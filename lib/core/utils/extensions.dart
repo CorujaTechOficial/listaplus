0166ecc5dcd74211566b550a0cbc62229846e5c9
AutoClaw Assistant <juninho@autoclaw.ai>
2026-05-24 17:49:43 -0300

    feat(core): add extensions utilities with TDD

---
+++ lib/core/utils/extensions.dart
@@ -0,0 +1,14 @@
+extension DateTimeExtensions on DateTime {
+  String toISODate() {
+    return '${year.toString().padLeft(4, '0')}-'
+        '${month.toString().padLeft(2, '0')}-'
+        '${day.toString().padLeft(2, '0')}';
+  }
+}
+
+extension StringExtensions on String {
+  String capitalize() {
+    if (isEmpty) return this;
+    return '${this[0].toUpperCase()}${substring(1)}';
+  }
+}
