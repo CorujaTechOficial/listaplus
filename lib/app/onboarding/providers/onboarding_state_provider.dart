44bc9f132550d68b5d75450e79bbc90c9ecb9a4a
AutoClaw Assistant <juninho@autoclaw.ai>
2026-05-28 11:32:56 -0300

    feat: add onboarding state providers

---
+++ lib/app/onboarding/providers/onboarding_state_provider.dart
@@ -0,0 +1,19 @@
+import 'package:flutter_riverpod/flutter_riverpod.dart';
+
+final onboardingSlideIndexProvider = StateProvider<int>((ref) => 0);
+
+final onboardingAiNameProvider = StateProvider<String>((ref) => '');
+
+final onboardingFavoriteFoodProvider = StateProvider<String>((ref) => '');
+
+final onboardingChatMessagesProvider = StateProvider<List<ChatMessage>>((ref) => []);
+
+class ChatMessage {
+  const ChatMessage({
+    required this.text,
+    required this.isUser,
+  });
+
+  final String text;
+  final bool isUser;
+}
